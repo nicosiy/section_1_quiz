a82e0531a57e0129191ba89d597886500d95583e
nicosiy <nicosiysoo@gmail.com>
2015-10-16 00:18:26 -0400

    Quiz commit

--- sketch.js
+++ sketch.js
@@ -1,16 +1,51 @@
 function setup() {
-	// create a place to draw
+	//Create Canvas
 	createCanvas(640, 360);
+	noStroke();
+
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 50);
+	// Background fill color & mouseIsPressed color
+	background(0, 0, 70);
+	if (mouseIsPressed) {
+		background(random(0, 200), random(0, 200), random(0, 200));
+	}
 
-	// set the drawing style
-	fill(100, 100, 100);
-	stroke(255, 255, 255);
+	// Draw Landscape
+	fill(10, 50, 25);
+	for (var x = 0; x < 10; x++) {
+		triangle((x * 65) - 50, 360, (x * 65) + 32, 200, (x * 65) + 114, 360);
+	}
 
-	// draw an ellipse
-	ellipse(320, 180, 100, 100);
+	fill(10, 110, 50);
+	rect(0, 280, 640, 370);
+
+	// UFO
+	fill(90, 90, 90);
+	ellipse(mouseX, mouseY - 60, 100, 50);
+	ellipse(mouseX, mouseY - 80, 50, 60);
+
+	// UFO Exterior
+	fill(75, 75, 75);
+	ellipse(mouseX - 50, mouseY - 60, 10, 10);
+	ellipse(mouseX - 25, mouseY - 60, 10, 10);
+	ellipse(mouseX, mouseY - 60, 10, 10);
+	ellipse(mouseX + 25, mouseY - 60, 10, 10);
+	ellipse(mouseX + 50, mouseY - 60, 10, 10);
+
+	// UFO mouseIsPressed color
+	if (mouseIsPressed) {
+
+		fill(random(0, 255), random(0, 255), random(0, 255));
+		ellipse(mouseX - 50, mouseY - 60, 10, 10);
+		ellipse(mouseX - 25, mouseY - 60, 10, 10);
+		ellipse(mouseX, mouseY - 60, 10, 10);
+		ellipse(mouseX + 25, mouseY - 60, 10, 10);
+		ellipse(mouseX + 50, mouseY - 60, 10, 10);
+		ellipse(mouseX, mouseY - 40, 30, 5);
+
+		// UFO Beam
+		ellipse(mouseX, 310, 65, 20);
+	}
 }
